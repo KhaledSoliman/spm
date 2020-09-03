c2a9c9bb5354cea76af043836ab91093d49b294c
Khaled Soliman <khaledsoli111@aucegypt.edu>
2020-09-03 15:57:41 +0200

    Update config.tcl

--- config.tcl
+++ config.tcl
@@ -1,7 +1,7 @@
 # Design
 set ::env(DESIGN_NAME) "spm"
 
-set ::env(VERILOG_FILES) [glob $::env(OPENLANE_ROOT)/designs/spm/src/*.v]
+set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
 
 set ::env(CLOCK_PERIOD) "10.000"
 set ::env(CLOCK_PORT) "clk"
@@ -14,7 +14,7 @@ set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 
 # set ::env(FP_PIN_ORDER_CFG) $::env(OPENLANE_ROOT)/designs/spm/pin_order.cfg
 
-set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
 	source $filename
 }
